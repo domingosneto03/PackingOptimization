2b63d3cbce8f6315165ca4b36049245da085bd87
domingosneto03 <domingos12126@gmail.com>
2025-05-15 18:48:04 +0100

    brute force corrected

--- BruteForceSolver.cpp
+++ BruteForceSolver.cpp
@@ -1,23 +1,25 @@
 #include "BruteForceSolver.h"
 #include <cmath>
 #include <iostream>
+#include <cstdint>
 
 std::pair<int, std::vector<int>> BruteForceSolver::solve(const TruckDataset& dataset) {
     int n = dataset.numPallets;
     int maxProfit = 0;
     std::vector<int> bestSubset;
-    int totalSubsets = 0;
+    uint64_t totalSubsets = 1ULL << n;
+    int exploredSubsets = 0;
 
-    // Iterate over all possible subsets (2^n possibilities)
-    for (int mask = 0; mask < (1 << n); ++mask) {
-        ++totalSubsets;
+    // Iterate over all possible subsets using 64-bit to avoid overflow
+    for (uint64_t mask = 0; mask < totalSubsets; ++mask) {
+        ++exploredSubsets;
 
         int totalWeight = 0;
         int totalProfit = 0;
         std::vector<int> currentSubset;
 
         for (int i = 0; i < n; ++i) {
-            if (mask & (1 << i)) {
+            if (mask & (1ULL << i)) {
                 totalWeight += dataset.pallets[i].weight;
                 totalProfit += dataset.pallets[i].profit;
                 currentSubset.push_back(dataset.pallets[i].id);
@@ -32,6 +34,6 @@ std::pair<int, std::vector<int>> BruteForceSolver::solve(const TruckDataset& dat
         }
     }
 
-    std::cout << "Total subsets explored: " << totalSubsets << "\n";
+    std::cout << "Total subsets explored: " << exploredSubsets << "\n";
     return {maxProfit, bestSubset};
 }
